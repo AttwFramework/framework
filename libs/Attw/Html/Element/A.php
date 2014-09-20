4c92a82a5c50401db596f2ac1681e8b8f21548e4
GabrielJMJ <gamjj74@hotmail.com>
2014-09-20 10:26:00 -0300

    Fixes bugs

---
+++ libs/Attw/Html/Element/A.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * AttwFramework
+ *
+ * @author Gabriel Jacinto <gamjj74@hotmail.com>
+ * @license MIT License
+ * @link http://attwframework.github.io
+*/
+
+namespace Attw\Html\Element;
+
+use Attw\Html\Element\AbstractHtmlElementNotLeaf;
+
+class A extends AbstractHtmlElementNotLeaf
+{
+    protected $elementName = 'a';
+
+    public function __construct($href = null, $target = null)
+    {
+        $href !== null ? $this->addAttr('href', $href) : null;
+        $taget !== null ? $this->addAttr('target', $target) : null;
+    }
+}
